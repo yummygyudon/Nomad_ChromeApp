b1292363990b77ac605693e9abc7e439f4473f41
DongGyu Jung <86935274+yummygyudon@users.noreply.github.com>
2022-05-14 10:26:11 +0000

    #2 Welcom to JavaScript Complete

---
+++ practice2.js
@@ -0,0 +1,59 @@
+/*
+
+const playerName = "Jung" ;
+const playerPoints = 121212 ; 
+const playerHandsome = true ;
+const playerFat = "little bit" ;
+*/
+/*
+//const player = ["Jung", 121212, true, "little bit"] ;
+const player = {
+  name : "Jung",
+  points : 10, 
+  handsome : true
+}
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+console.log(player.points);
+console.log(player["points"]);
+
+// 변경 Object가 const여도 가능 (그저 update 이기 때문)
+player.name = "Kim" ;
+console.log(player.name);
+
+// 추가
+player.lastName = "Donggyu";
+console.log(player);
+*/
+
+/*
+function sayHello() {
+  console.log("Hello");
+}
+for(let i = 0 ; i < 3 ; i++) {
+  sayHello();  
+}
+sayHello("Python"); // Hello
+sayHello("Java"); // Hello
+sayHello("JavaScript"); // Hello
+*/
+/*
+const player = {
+  name : "Jung",
+  sayHello : function(personToSayHello) {
+    console.log("hello " + personToSayHello+ "!! nice to meet you");
+  }
+};
+player.sayHello("Kim") ;
+*/
+/*
+const age = parseInt( prompt("Your age?") );
+// console.log(age) ;
+
+if (isNaN(age)) {
+  console.log("Wrong Value! Try Again!") ;
+}else {
+  console.log("Your age is "+age) ;
+}
+*/
